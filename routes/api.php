a60dc13130a535d30d16f9a70573f2165ec76712
auriyy <basmakovbogdan60@gmail.com>
2025-06-21 20:45:48 +0000

    Лабораторна 16. Nuxt, API, Blog Post View

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Api\Blog\PostController;
 
 Route::get('blog/posts', [PostController::class, 'index']);
 
+Route::get('blog/posts/{id}', [PostController::class, 'show']);
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
